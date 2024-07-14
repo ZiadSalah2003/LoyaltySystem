421a37b8be3456846e6e6e9fb877f0061328deea
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-07-14 04:23:16 +0300

    Update Authorization For Users

--- LoyaltySystem.Api/Controllers/AuthController.cs
+++ LoyaltySystem.Api/Controllers/AuthController.cs
@@ -9,16 +9,24 @@ namespace LoyaltySystem.Api.Controllers
 	public class AuthController : ControllerBase
 	{
 		private readonly IAuthService _authService;
-		public AuthController(IAuthService authService)
+		private readonly IEmailSender _emailSender;
+		public AuthController(IAuthService authService, IEmailSender emailSender)
         {
 			_authService = authService;
+			_emailSender = emailSender;
 
 		}
         [HttpPost("Login")]
 		public async Task<IActionResult> Login(LoginRequest request, CancellationToken cancellationToken)
 		{
 			var authResult = await _authService.GetTokenAsync(request.Email, request.Password, cancellationToken);
-			return authResult is null ? BadRequest("Invalid Email Or Password") : Ok(authResult);
+			if (authResult is null)
+				return BadRequest("Invalid Email Or Password");
+			var subject = "Login Notification";
+			var htmlMessage = $"Hello,<br>You have successfully logged in at {DateTime.UtcNow}.<br>Best Regards,<br>LoyaltySystem Team";
+			await _emailSender.SendEmailAsync(request.Email, subject, htmlMessage);
+
+			return  Ok(authResult);
 		}
 		[HttpPost("Register")]
 		public async Task<IActionResult> Register(RegisterRequest request, CancellationToken cancellationToken)
